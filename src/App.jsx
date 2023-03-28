1f151510b0936011792ae7cdcc379c5029aa9c07
Téo cathelot <t.cathelot.pro@gmail.com>
2023-03-28 16:42:29 +0200

    pokedexUdapte.1

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Bulbasaur from './components/PokemonCard.jsx'
+import PokemonCard from './components/PokemonCard.jsx'
 
 
 function App() {
 
   return (
     <div >
-      <Bulbasaur />
+      <PokemonCard />
 
     </div>
   )
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,44 @@
 
 
 
-function Bulbasaur() {
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+        imgSrc: "src/assets/151.png"
+    },
+    {
+        name: "charizard"
+
+    },
+];
+
+function PokemonCard() {
+
+    let pokemon = pokemonList[2];
+    let condition = pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>;
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-            <figcaption>Bulbasaur</figcaption>
+
+        <figure >
+
+
+            {condition}
+            <figCaption>{pokemon.name}</figCaption>
         </figure>
+
+
+
 
     )
 }
-export default Bulbasaur;
+
+
+
+
+
+export default PokemonCard;
 
